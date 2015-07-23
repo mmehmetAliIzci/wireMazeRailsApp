8001d9b1400166b0829fe8d19f15554237282f46
mmehmetAliIzci <mmehmetaliizci@hotmail.com>
2015-07-23 02:47:36 -0700

    Final

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :avatar, :styles => { :medium => "165x165", :thumb => "80x78" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
   belongs_to :user
